a602cc87dc571c0de009014ff396503964a2b910
Jonny Kemp <JonnyKemp@jonnys-mbp.sbank.uk.ibm.com>
2017-01-19 14:33:38 +0000

    create express server

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var express     = require('express'),
+    app         = express(),
+    bodyParser = require('body-parser'),
+    cfenv = require('cfenv');
+// get our request parameters7
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('./public'));
+var appEnv = cfenv.getAppEnv();
+app.listen(appEnv.port);
+console.log('Server running at port: ' + appEnv.port);
+module.exports = app;
+
